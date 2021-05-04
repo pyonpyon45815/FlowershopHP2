50d52959dd05a16a49d0481699bd65e0d1f2ae5c
pyonpyon45815 <gingiraharu2@gmail.com>
2021-05-04 22:25:08 +0900

    どうでしょう

---
+++ public/assets/js/script.js
@@ -0,0 +1,12 @@
+// ハンバーガーメニュー
+(function() {
+    $('#js-buttonHamburger').click(function() {
+        $('body').toggleClass('is-drawerActive');
+
+        if ($(this).attr('aria-expanded') == 'false') {
+            $(this).attr('aria-expanded', true);
+        } else {
+            $(this).attr('aria-expanded', false);
+        }
+    });
+})();

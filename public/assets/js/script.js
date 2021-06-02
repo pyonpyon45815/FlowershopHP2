ef7f67a3387b716212cacda4b8b7ad74d9cfb219
pyonpyon45815 <gingiraharu2@gmail.com>
2021-06-02 22:00:46 +0900

    iikannji

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,10 @@
 // ハンバーガーメニュー
 $(function() {
     $('#js-buttonHamburger').click(function() {
-        $('body').toggleClass('is-drawerActive');
 
         if ($(this).attr('aria-expanded') == 'false') {
             $(this).attr('aria-expanded', true);
+
         } else {
             $(this).attr('aria-expanded', false);
         }

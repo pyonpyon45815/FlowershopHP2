5ea097b576a643d7a1f76bc497672d25c85eaf1f
pyonpyon45815 <gingiraharu2@gmail.com>
2021-09-04 16:48:20 +0900

    iikannji

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -18,15 +18,6 @@ $(function() {
     });
 });
 
-//スムーススクロール
-$('#page-link a[href*="#"]').click(function() {
-    var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-    var pos = $(elmHash).offset().top - 90; //idの上部の距離を取得 -90は基準となる移動先から-90pxずらして表示する意味
-    $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-    return false;
-});
-
-
 
 // contactModal
 $(function() {
@@ -62,6 +53,7 @@ $(function() {
         }
     });
 });
+
 // スクロールヘッダー表示↔非表示
 var startPos = 0,
     winScrollTop = 0;
